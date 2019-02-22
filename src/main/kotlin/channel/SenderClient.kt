8497ca38fafd9e267f93c2467f7426606d8e9cf1
zhijia,.zhang <jiahut@gmail.com>
2019-02-22 10:15:30 +0800

    extend the channl support order and send strategy

--- src/main/kotlin/channel/SenderClient.kt
+++ src/main/kotlin/channel/SenderClient.kt
@@ -1,14 +1,39 @@
 package channel
 
 fun main(args: Array<String>) {
-    val senders = SenderManager.createSenders<String,String>()
+    val senders = SenderManager.buildSenderList<String, String>()
     senders.register(MailSender())
     senders.register(MsgSender())
 
-    senders
-      .select {
-        it.type().length > 1}
-      .send {
-        it.send("verification code 044166")
-      }
+    senders.select {
+        it.type().length > 1
+    }.sendAll {
+        it.send("verification code 100")
     }
+
+
+    println("-------------------------------")
+    val senders2 = SenderManager.buildSenderList<String, String>()
+    senders2.register(MailSender())
+    senders2.register(MsgSender())
+
+    senders2.select {
+        it.type().length > 1
+    }.sendByOrder {
+        it.send("verification code 110")
+    }
+
+
+
+    println("-------------------------------")
+    val senders3 = SenderManager.buildSenderList<String, String>()
+    senders3.register(MailSender())
+    senders3.register(MsgSender())
+
+    senders3.select {
+        it.type().length > 1
+    }.sendTailSuccess{
+        it.send("verification code 120")
+    }
+}
+
--- src/main/kotlin/channel/SenderInterface.kt
+++ src/main/kotlin/channel/SenderInterface.kt
@@ -3,7 +3,10 @@ package channel
 import java.util.*
 
 interface Sender<T,R> {
-
     fun send(msg: T): Optional<R>
     fun type():String
+}
+
+interface SendOrder {
+    fun order(): Int
 }
